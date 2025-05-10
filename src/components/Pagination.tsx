df6e65d3f54c8cd2870189183d63af3fc3cbdebf
Ibrahim Kedir <techofreact@gmail.com>
2025-05-10 13:40:32 +0300

    ui: minor ui adjustments.

--- src/components/Pagination.tsx
+++ src/components/Pagination.tsx
@@ -7,32 +7,37 @@ interface PaginationProps {
   onPageChange: (page: number) => void;
 }
 
-const Pagination: React.FC<PaginationProps> = ({ page, total, limit, onPageChange }) => {
+const Pagination: React.FC<PaginationProps> = ({
+  page,
+  total,
+  limit,
+  onPageChange,
+}) => {
   const totalPages = Math.ceil(total / limit);
   return (
     <div className="flex items-center justify-center gap-4 mt-8">
       <button
         onClick={() => onPageChange(page - 1)}
         disabled={page === 1}
-        className={`cursor-pointer px-4 py-2 rounded-lg font-medium transition ${
+        className={`px-4 py-2 rounded-lg font-medium transition ${
           page === 1
             ? "bg-gray-300 text-primary-gray cursor-not-allowed"
-            : "bg-primary text-white hover:bg-primary-hover"
+            : "bg-primary text-white hover:bg-primary-hover cursor-pointer"
         }`}
       >
         Previous
       </button>
       <span className="text-sm text-gray-700">
-        Page <span className="font-semibold text-purple-800">{page}</span> of {" "}
-        <span className="font-semibold text-purple-800">{totalPages}</span>
+        Page <span className="font-semibold text-primary">{page}</span> of{" "}
+        <span className="font-semibold text-primary">{totalPages}</span>
       </span>
       <button
         onClick={() => onPageChange(page + 1)}
         disabled={page >= totalPages}
-        className={`cursor-pointer px-4 py-2 rounded-lg font-medium transition ${
+        className={`px-4 py-2 rounded-lg font-medium transition ${
           page >= totalPages
             ? "bg-gray-300 text-primary-gray cursor-not-allowed"
-            : "bg-primary text-white hover:bg-primary-hover"
+            : "bg-primary text-white hover:bg-primary-hover cursor-pointer"
         }`}
       >
         Next
