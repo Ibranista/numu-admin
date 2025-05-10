165d664b5e4343dcf13020a6aa5ecb7531b4992c
Ibrahim Kedir <techofreact@gmail.com>
2025-05-10 10:05:01 +0300

    fix: update thunk api of therapists to accept formdata.

---
+++ src/features/therapists/thunk.api.ts
@@ -0,0 +1,29 @@
+import { createAsyncThunk } from "@reduxjs/toolkit";
+import api from "../../services/api.service";
+
+const feature = "/therapist";
+
+export const getTherapists = createAsyncThunk(
+    "therapists/getTherapists",
+    async (_, thunkAPI) => {
+        try {
+            const response = await api.get(`${feature}/`);
+            return response.data;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue(error.response?.data || error.message);
+        }
+    }
+);
+
+export const createTherapist = createAsyncThunk(
+    "therapists/createTherapist",
+    async (therapistData: FormData, thunkAPI) => {
+        try {
+            console.log("Therapist Data", therapistData);
+            const response = await api.post(`${feature}/`, therapistData);
+            return response.data;
+        } catch (error: any) {
+            return thunkAPI.rejectWithValue(error.response?.data || error.message);
+        }
+    }
+);
