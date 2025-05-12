c7becc74bd320b723f2a7cb2d8ebc4dbb891e2b9
Ibrahim Kedir <techofreact@gmail.com>
2025-05-12 11:41:03 +0300

    clear-up: remove unnecessary loading status showing.

--- src/components/ProtectedRoute.tsx
+++ src/components/ProtectedRoute.tsx
@@ -1,12 +1,10 @@
 import { Navigate } from "react-router-dom";
 import type { JSX } from "react";
-import Spinner from "./Spinner";
 import { useAuth } from "../hooks/withAuth";
 
 const ProtectedRoute = ({ children }: { children: JSX.Element }) => {
-  const { loading, isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-  if (loading) return <Spinner variation="screen-large" />;
   if (!isAuthenticated) return <Navigate to="/auth" replace />;
   return children;
 };
