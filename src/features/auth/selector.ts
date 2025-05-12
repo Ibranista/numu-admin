7a57ecbfafa877c31fb7b13e263d111f4f099c8a
Ibrahim Kedir <techofreact@gmail.com>
2025-05-12 11:42:20 +0300

    ft: minor fix and refactors.

--- src/features/auth/selector.ts
+++ src/features/auth/selector.ts
@@ -2,9 +2,9 @@ import { createSelector } from "@reduxjs/toolkit";
 import type { RootState } from "../../store/store";
 import { initialState } from "./initialState";
 
-const selectAuthSlice = (state: RootState) => state?.auth || initialState;
+const selectAuthSlice = (state: RootState) => state || initialState;
 
 export const selectAuthUser = createSelector(
     [selectAuthSlice],
-    state => state
+    state => state.auth
 );
--- src/ui/AuthRedirect.tsx
+++ src/ui/AuthRedirect.tsx
@@ -1,12 +1,10 @@
 import { Navigate } from "react-router-dom";
 import Auth from "./Auth";
 import { useAuth } from "../hooks/withAuth";
-import Spinner from "../components/Spinner";
 
 const AuthRedirect = () => {
-  const { loading, isAuthenticated } = useAuth();
+  const { isAuthenticated } = useAuth();
 
-  if (loading) return <Spinner variation="screen-large" />;
   if (isAuthenticated) return <Navigate to="/" replace />;
   return <Auth />;
 };
